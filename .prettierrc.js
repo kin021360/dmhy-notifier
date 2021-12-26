aa355ad1aaf1540c988d70d0dfc41a71f8a79bbd
Nathan Lam - desktop <kin021360@yahoo.com.hk>
2021-12-26 17:39:41 +0800

    setup ts eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: 'all',
+    singleQuote: true,
+    jsxBracketSameLine: true,
+    semi: true,
+    // endOfLine: 'auto',
+    importOrder: ['^(?!src/).*$', '^src/.*$'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
