c9b0e8ef1100c107bd6f99f7ac2e4882fe5a98ec
Nathan Lam - desktop <kin021360@yahoo.com.hk>
2021-12-26 19:56:20 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
-const path = require('path');
+import path from 'path';
 
-module.exports = {
-    tgBotToken: '' || process.env.tgBotToken, // Telegram bot API token
-    cachedbPath: path.resolve(__dirname, './leveldb/cachedb'),
-    userdbPath: path.resolve(__dirname, './leveldb/userdb'),
-    magnetHelperLink: '' || process.env.magnetHelperLink,
-};
+export const tgBotToken = '' || process.env.tgBotToken; // Telegram bot API token
+export const cachedbPath = path.resolve(__dirname, './leveldb/cachedb');
+export const userdbPath = path.resolve(__dirname, './leveldb/userdb');
+export const magnetHelperLink = '' || process.env.magnetHelperLink;
+
+export const esLogEndpoint = process.env.esLogEndpoint || '';
+export const esLogIndex = process.env.esLogIndex || '';
